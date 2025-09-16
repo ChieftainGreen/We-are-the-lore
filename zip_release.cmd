b9a82345c8319c89d0d096ee503a04f5a5207e78
Igor Perfilov <6770145@gmail.com>
2025-09-16 21:49:47 +0300

    readme processed in laboratory

--- zip_release.cmd
+++ zip_release.cmd
@@ -1,2 +1,3 @@
-REM change version number! 
-tar -a -c --exclude=".git*" --exclude="*.zip" --exclude="*.cmd" -f We-are-the-lore.0.3.zip ..\We-are-the-lore
+REM pack mod to release zip file
+REM Don't forget to change version number! 
+tar -a -c --exclude=".git*" --exclude="*.zip" --exclude="*.cmd" -f We-are-the-lore.0.5.zip ..\We-are-the-lore
